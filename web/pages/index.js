16157ec70b419dfc39e0925735a03d4e3938a532
nahom balcha <nahom.projectmanagement@gmail.com>
2021-07-23 00:47:35 -0700

    index page done...

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div className="bg-red-400">let's start...</div>;
+  return (
+    <div
+      className="h-screen flex justify-center items-center bg-fb-bg"
+      style={{ backgroundImage: "url(facebook.jpg)", backgroundSize: "cover" }}
+    >
+      <button className="px-2 py-2 bg-white rounded-lg  flex items-center hover:bg-blue-50">
+        <img src="icons8-facebook.svg" alt="logo" className="h-10 mr-2 " />
+        <span className="font-bold   text-blue-700">Sign In with facebook</span>
+      </button>
+    </div>
+  );
 }
